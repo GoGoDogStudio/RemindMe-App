6ee51614ed6f9cbc9e9f654839e7ed8d892694f3
GoGoDogStudio <63595952+GoGoDogStudio@users.noreply.github.com>
2020-04-15 22:39:44 +0300

    Created Toolbar

--- app/src/main/java/com/gogodogstudio/remindmeapp/MainActivity.java
+++ app/src/main/java/com/gogodogstudio/remindmeapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.gogodogstudio.remindmeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
