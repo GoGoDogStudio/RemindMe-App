bdbcc5af9cf00cf20e74cf9afb9a45ee51edfa59
GoGoDogStudio <63595952+GoGoDogStudio@users.noreply.github.com>
2020-04-16 13:17:37 +0300

    Added NavigationDrawer

--- app/src/main/java/com/gogodogstudio/remindmeapp/MainActivity.java
+++ app/src/main/java/com/gogodogstudio/remindmeapp/MainActivity.java
@@ -1,25 +1,34 @@
 package com.gogodogstudio.remindmeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private MaterialToolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initToolBar();
+
+
     }
 
+
+
     private void initToolBar() {
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar = (MaterialToolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
